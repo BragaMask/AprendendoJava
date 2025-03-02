73621672cb7adc3b7c6f92e96f7fd560c7af9e26
Braga <BragaPensador@gmail.com>
2025-03-02 17:59:54 -0400

    Hello World!

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+public class PrimeiroPrograma //Classe
+{ 
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, Mundo!"); //Eita comando GIGANTE!
+        System.out.println("Meu primeiro programa em Java!");
+        System.out.println("Me chamo Braga!");
+    } //Método
+}
+//Método principal dentro de uma classe pública 
